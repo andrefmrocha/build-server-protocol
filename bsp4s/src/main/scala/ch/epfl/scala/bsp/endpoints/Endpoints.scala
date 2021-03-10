ff6610463ea22e0d5956c4d23f4621b28113416a
Andre Rocha <andrefmrocha@live.com.pt>
2021-03-10 16:48:12 +0000

    Add c++ options request

--- bsp4s/src/main/scala/ch/epfl/scala/bsp/endpoints/Endpoints.scala
+++ bsp4s/src/main/scala/ch/epfl/scala/bsp/endpoints/Endpoints.scala
@@ -56,6 +56,11 @@ trait BuildTarget {
     extends Endpoint[JavacOptionsParams, JavacOptionsResult](
       "buildTarget/javacOptions"
     )
+  // C++ specific endpoints
+  object cppOptions
+    extends Endpoint[CppOptionsParams, CppOptionsResult](
+      "buildTarget/cppOptions"
+    )
 }
 
 object Workspace extends Workspace
